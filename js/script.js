faecb1ccfee5560400996ade4ed4cf5660a3c37b
bp82125 <103567877+bp82125@users.noreply.github.com>
2023-10-29 22:30:10 +0700

    - made carousel not auto playing. - added changing nav bar colors

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
-  $(window).scroll(function () {
-      if ($(this).scrollTop()) {
-          $('nav').addClass('sticky');
-      } else {
-          $('nav').removeClass('sticky');
-      }
-  });
+    $(window).scroll(function () {
+        if ($(this).scrollTop()) {
+            $('nav').addClass('sticky');
+            $('nav a').removeClass('text-white');
+            $('nav a').addClass('text-black');
+        } else {
+            $('nav').removeClass('sticky');
+            $('nav a').removeClass('text-black');
+            $('nav a').addClass('text-white');
+        }
+    });
 });
